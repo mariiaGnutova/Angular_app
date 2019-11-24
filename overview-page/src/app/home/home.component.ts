7a61bbdd50dd02b39844a4403fa0c40c0cae0232
@mariia.gnutova <m.gnutova@myphotobook.de>
2019-11-24 17:45:25 +0100

    added scrolling and descriptions

--- overview-page/src/app/home/home.component.ts
+++ overview-page/src/app/home/home.component.ts
@@ -4,17 +4,33 @@ import { ApiService } from '../api.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.sass']
+  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
   jobs = [];
+  description ='';
+  attachments = [];
+  title = '';
   constructor(private apiService: ApiService) { }
 
   ngOnInit() {
 		this.apiService.get().subscribe((data: any[])=>{  
 			console.log(data);  
 			this.jobs = data;  
-		})  
-	}
+    })  
+    
+  }
+  showMore(job){
+    var a = this.jobs["body"][0]["description"];
+    console.log("first.job " + this.jobs["body"][0]["description"]);
+    this.description = job["description"];
+    this.attachments = job["attachments"];
+    this.title = job["title"];
+  }
+
+  getDescription(){
+    return this.jobs["body"][0]["description"];
+  }
+  
 
 }
